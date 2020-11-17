d75e6dcf166316d117b35c5e11c4ad0df56bf63e
Johnson Chen <johnson880319@hotmail.com>
2020-11-18 00:57:26 +0800

    added apriltag detection and stop function

---
+++ catkin_ws/src/spin_test/src/detection.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import rospy
+from apriltag_ros.msg import AprilTagDetectionArray
+from duckietown_msgs import BoolStamped
+
+class detection:
+    def __init__(self):
+        self.detected = False
+        self.pub = rospy.Publisher('/duck5/wheels_driver_node/emergency_stop', BoolStamped, queue_size=10)
+        self.sub = rospy.Subscriber('/tag_detections', AprilTagDetectionArray, self.detectionCallback)
+        self.pubStop()
+
+    def detectionCallback(self, data):
+        for detection in data.detections:
+            for id_ in detection.id:
+                if id_ == 2:
+                    rospy.loginfo("detected tag id==2, stopping duckiebot")
+                    self.detected = True
+                    return
+        self.detected = False
+    
+    def pubStop(self):
+        while self.detected == True:
+            emstop = BoolStamped()
+            emstop.data = True
+            pub.publish(emstop)
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('tag_detector')
+        spin_test()
+    except rospy.ROSInterruptException:
+        pass
