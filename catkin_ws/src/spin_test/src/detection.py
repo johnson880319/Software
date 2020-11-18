287f4c6ee20d8ed6e047c0c24658aabac31dbc1c
Johnson Chen <johnson880319@hotmail.com>
2020-11-18 09:46:01 +0800

    updated detection logic

--- catkin_ws/src/spin_test/src/detection.py
+++ catkin_ws/src/spin_test/src/detection.py
@@ -6,29 +6,28 @@ from duckietown_msgs.msg import BoolStamped
 class Detection:
     def __init__(self):
         self.detected = False
+        self.lastDetected = False
         self.pub = rospy.Publisher('/duck5/wheels_driver_node/emergency_stop', BoolStamped, queue_size=10)
         self.sub = rospy.Subscriber('/tag_detections', AprilTagDetectionArray, self.detectionCallback)
         self.pubStop()
         rospy.spin()
 
     def detectionCallback(self, data):
+        self.detected = False
         for detection in data.detections:
             for id_ in detection.id:
                 if id_ == 2:
                     rospy.loginfo("detected tag id==2, stopping duckiebot")
                     self.detected = True
-                    return
-        self.detected = False
+        if self.detected != self.lastDetected:
+            self.pubStop()
+        self.lastDetected = self.detected
     
     def pubStop(self):
-        rate = rospy.Rate(1)
-        while not rospy.is_shutdown():
-            if self.detected == True:
-                rospy.loginfo("publishing emergency_stop")
-                emstop = BoolStamped()
-                emstop.data = True
-                self.pub.publish(emstop)
-                rate.sleep()
+        rospy.loginfo("publishing emergency_stop")
+        emstop = BoolStamped()
+        emstop.data = True
+        self.pub.publish(emstop)
 
 if __name__ == '__main__':
     try:
